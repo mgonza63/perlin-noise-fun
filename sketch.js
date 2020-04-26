ee3e8f7bc7ca233cfcdb4ef695eae3573521b198
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-26 01:51:02 -0500

    added a start variable to show a moving graph

--- sketch.js
+++ sketch.js
@@ -20,6 +20,10 @@
 
 // // circle
 
+var inc = 0.01
+
+var start = 0;
+
 function setup() {
     createCanvas(400, 400);
 }
@@ -29,14 +33,16 @@ function draw() {
     stroke(255);
     noFill()
     beginShape()
-    var xoff = 0;
+    var xoff = start;
     for (var x = 0; x < width; x++) {
         stroke(255);
         var y = noise(xoff) * height;
         vertex(x, y);
 
-        xoff += 0.02
+        xoff += inc
     }
     endShape();
-    noLoop();
+
+    start += inc
+
 }
