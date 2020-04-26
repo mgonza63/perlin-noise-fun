0f0cab322eed7a4379247aa3829ced03612240df
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-26 02:53:07 -0500

    created rough textures that when put together they sides match

--- sketch.js
+++ sketch.js
@@ -18,31 +18,62 @@
 //     ellipse(x, y, 24, 24)
 // }
 
-// // circle
+// // <-- Moving Circle -->
+
+// var inc = 0.01
+
+// var start = 0;
+
+// function setup() {
+//     createCanvas(400, 400);
+// }
+
+// function draw() {
+//     background(51);
+//     stroke(255);
+//     noFill()
+//     beginShape()
+//     var xoff = start;
+//     for (var x = 0; x < width; x++) {
+//         stroke(255);
+//         var y = noise(xoff) * height;
+//         vertex(x, y);
+
+//         xoff += inc
+//     }
+//     endShape();
+
+//     start += inc
+
+// }
+
+// // <-- Moving Graph -->
 
 var inc = 0.01
 
-var start = 0;
-
 function setup() {
     createCanvas(400, 400);
+    
 }
 
 function draw() {
-    background(51);
-    stroke(255);
-    noFill()
-    beginShape()
-    var xoff = start;
-    for (var x = 0; x < width; x++) {
-        stroke(255);
-        var y = noise(xoff) * height;
-        vertex(x, y);
 
-        xoff += inc
+    var yoff = 0;
+    var xoff = 0;
+    loadPixels();
+    for (var y = 0; y < width; y++) {
+        for (var x = 0; x < width; x++) {
+            var index = (x + y * width) * 4;
+            var r = noise(xoff) * 255;
+            pixels[index + 0] = r;
+            pixels[index + 1] = r;
+            pixels[index + 2] = r;
+            pixels[index + 3] = r;
+
+            xoff += 0.01
+        }
     }
-    endShape();
-
-    start += inc
-
+    updatePixels();
 }
+
+// <-- Moving Graph -->
