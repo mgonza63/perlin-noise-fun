06a17a9024cab2baed4c94a682bea1475169f027
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-26 01:46:06 -0500

    added a for loop to create a perlin noise graph

--- sketch.js
+++ sketch.js
@@ -1,6 +1,24 @@
+// var xoff1 = 0;
+// var xoff2 = 1000;
 
-var xoff1 = 0;
-var xoff2 = 1000;
+// function setup() {
+//     createCanvas(400, 400);
+// }
+
+// function draw() {
+//     background(51);
+
+
+//     // var x = random(width);
+//     var x = map(noise(xoff1), 0, 1, 0, width); // change noise to 1 for single frames
+//     var y = map(noise(xoff2), 0, 1, 0, height);
+
+//     xoff1 += 0.01
+//     xoff2 += 0.01
+//     ellipse(x, y, 24, 24)
+// }
+
+// // circle
 
 function setup() {
     createCanvas(400, 400);
@@ -8,15 +26,17 @@ function setup() {
 
 function draw() {
     background(51);
+    stroke(255);
+    noFill()
+    beginShape()
+    var xoff = 0;
+    for (var x = 0; x < width; x++) {
+        stroke(255);
+        var y = noise(xoff) * height;
+        vertex(x, y);
 
-
-    // var x = random(width);
-    var x = map(noise(xoff1), 0, 1, 0, width); // change noise to 1 for single frames
-    var y = map(noise(xoff2), 0, 1, 0, height);
-
-    xoff1 += 0.02
-    xoff2 += 0.02
-    ellipse(x, y, 24, 24)
+        xoff += 0.02
+    }
+    endShape();
+    noLoop();
 }
-
-// circle
