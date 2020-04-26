c1c425d4eeca3030595cad29f68548d06d1a430c
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-26 14:10:49 -0500

    created the perlin noise loops to simulate flying over terrain

--- sketch.js
+++ sketch.js
@@ -104,10 +104,23 @@ function setup() {
 }
 
 function draw() {
+    flying -= 0.1;
+    var yoff = flying;
+    for (var y = 0; y < rows; y++) {
+        var xoff = 0;
+    
+        for (var x = 0; x < cols; x++) {
+            terrain[x][y] = map(noise(xoff,  yoff), 0, 1, -100, 100)
+            xoff += 0.2
+    }
+    yoff += 0.2
+}
+
     background(0);
     translate(0, 50)
     rotateX(PI / 3);
     fill(200, 200, 200, 50);
+
     translate(-w / 2, -h / 2);
 
     for (var y = 0; y < rows - 1; y++) {
