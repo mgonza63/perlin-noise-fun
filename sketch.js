76ed52ba9c09c8c6181161ed133c8665e34f1c54
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-26 13:59:14 -0500

    added z values to prepare for the perlin noise in the terrain

--- sketch.js
+++ sketch.js
@@ -83,23 +83,40 @@
 
 var cols, rows;
 var scl = 20;
+var w = 1400;
+var h = 1000;
+
+var flying = 0;
+
+var terrain = [];
 
 function setup() {
     createCanvas(600,600, WEBGL);
-    var w = 600;
-    var h = 600;
     cols = w / scl;
     rows = h / scl;
+
+    for (var x = 0; x < cols; x++) {
+        terrain[x] = [];
+        for (var y = 0; y < rows; y++) {
+            terrain[x][y] = 0;
+        }
+    }
 }
 
 function draw() {
     background(0);
+    translate(0, 50)
+    rotateX(PI / 3);
+    fill(200, 200, 200, 50);
+    translate(-w / 2, -h / 2);
 
-    for (var x = 0; x< cols; x++) {
-        for (var y = 0; y< rows; y++) {
-
-
+    for (var y = 0; y < rows - 1; y++) {
+        beginShape(TRIANGLE_STRIP)
+        for (var x = 0; x < cols; x++) {
+            vertex(x * scl, y * scl,  terrain[x][y])
+            vertex(x * scl, (y + 1) * scl, terrain[x][y + 1])
 
         }
+        endShape();
     }
 }
