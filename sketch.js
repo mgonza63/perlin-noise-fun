4f2a5c43bd2dfd03e3f783ae0fb6f39c38837459
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-04-26 03:02:39 -0500

    added y offset to create a blur effect

--- sketch.js
+++ sketch.js
@@ -59,19 +59,22 @@ function setup() {
 function draw() {
 
     var yoff = 0;
-    var xoff = 0;
+    
     loadPixels();
     for (var y = 0; y < width; y++) {
+        var xoff = 0;
         for (var x = 0; x < width; x++) {
             var index = (x + y * width) * 4;
-            var r = noise(xoff) * 255;
+            var r = noise(xoff, yoff) * 255;
             pixels[index + 0] = r;
             pixels[index + 1] = r;
             pixels[index + 2] = r;
-            pixels[index + 3] = r;
+            pixels[index + 3] = 255;
 
-            xoff += 0.01
+            xoff += inc
+            
         }
+        yoff += inc
     }
     updatePixels();
 }
